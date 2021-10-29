0770b95789d88dca0bf588cb87fbc90ce689d637
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-29 22:21:50 +0800

    add new notes

--- src/app/pages/home-notes/home-notes.component.ts
+++ src/app/pages/home-notes/home-notes.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { TokenService } from './../../service/token.service';
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/service/note.service';
 
 @Component({
   selector: 'app-home-notes',
@@ -6,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-notes.component.scss'],
 })
 export class HomeNotesComponent implements OnInit {
-  constructor() {}
+  notes = [];
+  newNote = {
+    title: '',
+    description: '',
+    effort: '',
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    private note: NoteService,
+    private token: TokenService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.token.getToken()) this.router.navigate(['/login']);
+  }
+
+  onSubmit(): void {
+    const { title, description, effort } = this.newNote;
+    this.note.addNewNotes(title, description, parseInt(effort)).subscribe(
+      (data) => {
+        this.notes = data;
+        this.newNote.title = '';
+        this.newNote.description = '';
+        this.newNote.effort = '';
+      },
+      (err) => {
+        console.log('insert new note into database failed');
+      }
+    );
+  }
 }
