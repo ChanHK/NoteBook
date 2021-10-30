dade22e1c9144e210c7d3c2d9ec9e0028dd0fdcd
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-30 09:48:02 +0800

    Update note.ts

--- src/app/model/note.ts
+++ src/app/model/note.ts
@@ -1,41 +1,13 @@
 export class Note {
-  private _title: string;
-  private _description: string;
-  private _effort: number;
-  private id: string;
+  public title: string;
+  public description: string;
+  public effort: number;
+  public _id: string;
 
   constructor(title: string, description: string, effort: number, id: string) {
-    this._title = title;
-    this._description = description;
-    this._effort = effort;
-    this.id = id;
-  }
-
-  get title(): string {
-    return this._title;
-  }
-  set title(value: string) {
-    this._title = value;
-  }
-
-  get description(): string {
-    return this._description;
-  }
-  set description(value: string) {
-    this._description = value;
-  }
-
-  get effort(): number {
-    return this._effort;
-  }
-  set effort(value: number) {
-    this._effort = value;
-  }
-
-  get _id(): string {
-    return this.id;
-  }
-  set _id(value: string) {
-    this.id = value;
+    this.title = title;
+    this.description = description;
+    this.effort = effort;
+    this._id = id;
   }
 }
