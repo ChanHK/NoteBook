845a9228c45e998bd33c1f309cbaff51d780fee8
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-30 10:38:54 +0800

    show user score and name

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  userInfo: User = {
+    username: '',
+    score: 0,
+  };
+  constructor(private userSer: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userSer.getUserInfo().subscribe((data) => {
+      this.userInfo = data;
+    });
+  }
 }
--- src/app/pages/home-home/home-home.component.ts
+++ src/app/pages/home-home/home-home.component.ts
@@ -1,4 +1,6 @@
+import { UserService } from './../../service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-home-home',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-home.component.scss'],
 })
 export class HomeHomeComponent implements OnInit {
-  constructor() {}
+  userInfo: User = {
+    username: '',
+    score: 0,
+  };
 
-  ngOnInit(): void {}
+  constructor(private userSer: UserService) {}
+
+  ngOnInit(): void {
+    this.userSer.getUserInfo().subscribe((data) => {
+      this.userInfo = data;
+    });
+  }
 }
