67b8cd0e0a5ad81d4c35ed596a7992bea1d044c2
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-30 13:17:43 +0800

    create client-server (part 2) remove src

--- src/app/model/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  public username: string;
-  public score: number;
-
-  constructor(username: string, score: number) {
-    this.username = username;
-    this.score = score;
-  }
-}
