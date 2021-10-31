eb16f9ff64187b1863d08e4ca160e79e27330467
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-31 21:56:02 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use("/api/user", user);
 
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "../client/dist/client"));
+  app.use(express.static(path.join(__dirname + "../client/dist/client")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/client/index.html"));
