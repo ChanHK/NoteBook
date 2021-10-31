54b457571b21427a8284b67390d49f4b64dc7b24
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-31 21:37:46 +0800

    deploy adjustment

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
